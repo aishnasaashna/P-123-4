d0fc7213cf2018230764bc7492d3669e00157d8c
aishnasaashna <83213468+aishnasaashna@users.noreply.github.com>
2022-04-01 17:31:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    canvas=createCanvas(500,500);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#f0b30c');
+}
+
+function modelLoaded(){
+console.log('poseNet is working');
+}
+
+function gotPoses(results){
+    if(results.lenght>0)
+    {
+        console.log(results);
+    }
+}
